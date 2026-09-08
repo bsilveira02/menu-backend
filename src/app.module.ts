61611a74e6db545c1bac1b737ff8f3fd71d0e889
Libório Júnior <libajunior@gmail.com>
2026-09-08 08:56:54 -0300

    feat: Aula 01/09

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const databaseURL = configService.get<string>('DATABASE_URL');
+        const dbSchema = configService.get<string>('DATABASE_SCHEMA', 'public');
+
+        if (!databaseURL) {
+          throw new Error('A variável de ambiente DATABASE_URL não foi encontrada!');
+        }
+
+        return {
+          type: 'postgres',
+          url: databaseURL,
+          schema: dbSchema,
+          autoLoadEntities: true,
+          synchronize: true,
+          ssl: false
+        }
+      }
+    })
+  ],
   controllers: [],
   providers: [],
 })
